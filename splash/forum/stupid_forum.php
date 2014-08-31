e36fa827e5e881498f55b72ec49db0cd1b6ebb32
panayotis antoniadis <panayotis@nethood.org>
2014-08-31 22:55:12 +0200

    cleaning up

---
+++ splash/forum/stupid_forum.php
@@ -0,0 +1,19 @@
+// stupid_forum.php
+<?php
+
+$data = array();      // array to pass back data
+
+$text1 = $_POST['text1'];
+$text2 = $_POST['text2'];
+$text3 = $_POST['text3'];
+
+file_put_contents('./text1.txt', $text1);
+file_put_contents('./text2.txt', $text2);
+file_put_contents('./text3.txt', $text3);
+
+$data['success'] = true;
+$data['message'] = 'Success!';
+
+echo json_encode($data);
+
+?>
